c499f40ce20686715b9efacbbc6acf78917a22ed
TyrusFernandes <st10365737@vcconnect.edu.za>
2025-09-13 22:37:35 +0200

    AcadenceApp-WorkingAndFilledNavbar

--- app/src/main/java/com/example/acadenceapp/DashboardActivity.kt
+++ app/src/main/java/com/example/acadenceapp/DashboardActivity.kt
@@ -1,6 +1,9 @@
 package com.example.acadenceapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,5 +13,22 @@ class DashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         // Set the content view to the activity_main.xml layout file.
         setContentView(R.layout.activity_dashboard)
+        val btnSettings: ImageButton = findViewById(R.id.btnSettings)
+        val btnForYou: Button = findViewById(R.id.btnArticle)
+        val btnJobs: Button = findViewById(R.id.btnJobs)
+
+        btnSettings.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnForYou.setOnClickListener {
+            val intent = Intent(this, ForYouPageActivity::class.java)
+            startActivity(intent)
+        }
+        btnJobs.setOnClickListener {
+            val intent = Intent(this, JobActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/acadenceapp/JobActivity.kt
@@ -0,0 +1,20 @@
+package com.example.acadenceapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class JobActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_job)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.JobPage)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
