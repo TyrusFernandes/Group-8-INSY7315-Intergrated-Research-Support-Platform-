b5cf6f2b6f7b2766a09041a1946494bc980a8128
Nail Arslan <st10342009@vcconnect.edu.za>
2025-10-22 00:39:53 +0200

    made a few minor fixes

---
+++ Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AcadenceWebApp.Models
+{
+    public class UserModel
+    {
+        public string Uid { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string Role { get; set; }
+    }
+
+}
