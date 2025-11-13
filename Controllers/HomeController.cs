e8a0f2cde916dafe07bbdc298813821f95cc9b37
OratileKodisang <st10363214@vcconnect.edu.za>
2025-11-14 00:01:22 +0200

    FAQ and Privacy pages implemented, Sign Up UI updated

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace AcadenceWebApp.Controllers
             return View();
         }
 
+        public IActionResult HelpCentre()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
